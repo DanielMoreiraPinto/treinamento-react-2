38b6ba12129c3be94226b82a803a5e84cc3480b4
Gabriel S <gabrielssc.ti@gmail.com>
2020-04-28 22:50:01 -0300

    Finished

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
+import * as React from 'react';
 import './App.css';
+import Homepage from './pages/Homepage/Homepage.component';
+import Restaurant from './pages/Restaurant/Restaurant.component';
+import {Switch, Route} from 'react-router-dom'
 
 function App() {
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    //<div class="jumbotron-fluid"></div>
+    <Switch>
+      <Route exact path='/' component={Homepage}/>
+      <Route exact path='/restaurants/:handle' component={Restaurant}/>
+    </Switch>
   );
 }
 
